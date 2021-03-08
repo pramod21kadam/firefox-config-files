e612841c7b0591a36c32ec22b4e22d700e49af49
pramod kadam <pramod21dkadam@gmail.com>
2021-03-08 12:05:58 +0530

    updates

--- startpage/scripts/search.js
+++ startpage/scripts/search.js
@@ -12,29 +12,3 @@ function toggleEngine() {
   }
   document.getElementById("textField").innerHTML = null;
 }
-
-// let set_weather = () => {
-//   fetch('https://wttr.in/?format="%C:%f"')
-//   .then(response => response.text())
-//   .then(
-//       data => {
-//         data = data.replaceAll('"', '').split(':');
-//         document.getElementById('temprature').innerHTML = data[1].substring(1, data[1].length-2) + "&deg;c";
-//         document.getElementById('weatherdescription').innerHTML = data[0];
-//         localStorage.setItem('temperature', data[1].substring(1, data[1].length-2) + "&deg;c");
-//         localStorage.setItem('weatherdescription', data[0]);
-//       }
-//   ).catch(error => {
-//     var temperature = localStorage.getItem('temperature');
-//     var description = localStorage.getItem('weatherdescription');
-//     if (!temperature && !description){
-//       document.getElementById('temprature').innerHTML = "Weather Not Available";
-//     }
-//     else{
-//       document.getElementById('temprature').innerHTML = temperature;
-//       document.getElementById('weatherdescription').innerHTML = description;
-//     }
-//   });
-// }
-
-// set_weather();
